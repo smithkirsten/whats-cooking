334f7d78bdb4783ea5a1aaf1b1ed5558fdeb16ff
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-07 17:05:16 -0400

    Fix imports and add jsx wrapper git add src/Components/App.js git add src/Components/App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 import { Route, Routes } from "react-router-dom"
-import { Login } from './Login/Login'
-import { Dashboard } from './Dashboard/Dashboard'
+import Login from './Login/Login'
+import Dashboard from './Dashboard/Dashboard'
 
 function App() {
-  
-
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 }
 
