f95f36d64265ffa17653ea50148b71df7ce8847a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-07 17:12:49 -0400

    Add user, recipe, and ingredients state to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-//import { useState } from 'react';
+import { useState } from 'react';
 import { Route, Routes } from "react-router-dom"
 import Login from './Login/Login'
 import Dashboard from './Dashboard/Dashboard'
 
 function App() {
+  const [user, setUser] =  useState({})
+  const [recipes, setRecipes] = useState([])
+  const [ingredients, setIngredients] = useState([])
 
   return (
     <>
