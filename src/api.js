cc71443bab83495850aca41c53dee2ae252eadc3
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-07 22:20:01 -0400

    Add try and catch to api call

--- src/api.js
+++ src/api.js
@@ -1,5 +1,11 @@
 const getCall = async(endpoint) => {
-  return await fetch(`http://localhost:3001/api/v1/${endpoint}`)
+  try {
+    const response = await fetch(`http://localhost:3001/api/v1/${endpoint}`)
+    return await response.json()
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 }
 
 export { getCall }
