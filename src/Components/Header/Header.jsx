20b113e5be8e8787a071e79452b7b040a6d59e3e
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-10 15:36:12 -0400

    Add Header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+function Header({ user }) {
+
+  return (
+    <header>
+
+    </header>
+  );
+}
+
+export default Header;
