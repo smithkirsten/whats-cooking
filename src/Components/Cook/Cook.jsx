dcf62cbef322d014c23c2ca8cf874af655121289
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-10 16:57:02 -0400

    Add Pantry and Cook pages

---
+++ src/Components/Cook/Cook.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import Header from '../Header/Header'
+
+
+function Cook({ }) {
+ 
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default Cook;
