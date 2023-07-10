0e3c2fd8aa5b75947a50308084f78bd537038fa3
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-10 16:57:24 -0400

    Add Pantry page

---
+++ src/Components/Pantry/Pantry.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import Header from '../Header/Header'
+
+
+function Pantry({ }) {
+ 
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default Pantry;
