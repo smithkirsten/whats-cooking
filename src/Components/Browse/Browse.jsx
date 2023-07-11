0f1f1887435e58b236c34c7707adad8e01c5d672
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-11 11:07:11 -0400

    Add recipe cards to browse

--- src/Components/Browse/Browse.jsx
+++ src/Components/Browse/Browse.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import Header from '../Header/Header'
+import RecipeCard from "../RecipeCard/RecipeCard";
 
 
-function Browse({ }) {
+function Browse({user, setUser, recipes }) {
  
   return (
     <main>
-
+      <Header user={user} setUser={setUser} />
+      <nav>
+        
+      </nav>
+      <section>
+        {recipes.map(r => <RecipeCard recipe={r}/>)}
+      </section>
     </main>
   );
 }
