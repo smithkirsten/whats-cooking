3fb494508d4fe9a6c3788d2fe0a90af5aeba6d49
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-11 11:13:09 -0400

    Add nav bar for easy tag filtering

--- src/Components/Browse/Browse.jsx
+++ src/Components/Browse/Browse.jsx
@@ -4,12 +4,19 @@ import RecipeCard from "../RecipeCard/RecipeCard";
 
 
 function Browse({user, setUser, recipes }) {
+
+  const filterByTag = (tag) => {
+
+  }
  
   return (
     <main>
       <Header user={user} setUser={setUser} />
-      <nav>
-        
+      <nav className={'tag-nav'}>
+        <button className={'tag-nav-button'} onClick={() => {filterByTag('breakfast')}}>BREAKFAST</button>
+        <button className={'tag-nav-button'} onClick={() => {filterByTag('lunch')}}>LUNCH</button>
+        <button className={'tag-nav-button'} onClick={() => {filterByTag('dinner')}}>DINNER</button>
+        <button className={'tag-nav-button'} onClick={() => {filterByTag('dessert')}}>DESSERT</button>
       </nav>
       <section>
         {recipes.map(r => <RecipeCard recipe={r}/>)}
