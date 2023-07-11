95ccb5b59b0dd54342620a7accce292aa11e0e8a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-11 10:26:29 -0400

    Add shell for RecipeCard

---
+++ src/Components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import Header from '../Header/Header'
+
+
+function RecipeCard({ }) {
+ 
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default RecipeCard;
