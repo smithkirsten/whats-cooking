00aa878130b099367fa84c8e82bd79b2edcca561
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-13 14:26:46 -0400

    Move renderFilter to FilterNav

--- src/Components/Browse/Browse.jsx
+++ src/Components/Browse/Browse.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import './Browse.css'
 import Header from '../Header/Header'
+import FilterNav from '../FilterNav/FilterNav'
 import RecipeCard from "../RecipeCard/RecipeCard";
 
 
@@ -9,39 +11,12 @@ function Browse({user, setUser, recipes }) {
   const filterByTag = (tag) => {
     setFilter(tag)
   }
-  const renderFilters = () => {
-    if(filter !== 'breakfast' && filter !== 'lunch' && filter !== 'dinner' && filter !== '') {
-      //once active class is applied to nav buttons, can simplify this conditional into checking whether any of the buttons are active
-      return filter
-    }
-  }
+
  
   return (
     <main>
       <Header user={user} setUser={setUser} />
-      <nav className={'tag-nav'}>
-        <button 
-          className={'tag-nav-button'} 
-          onClick={() => {filterByTag('breakfast')}}
-          style={{ color: filter === 'breakfast' ? 'white' : '#292929' }}
-        >BREAKFAST</button>
-        <button 
-          className={'tag-nav-button'} 
-          onClick={() => {filterByTag('lunch')}}
-          style={{ color: filter === 'lunch' ? 'white' : '#292929' }}
-        >LUNCH</button>
-        <button 
-          className={'tag-nav-button'} 
-          onClick={() => {filterByTag('dinner')}}
-          style={{ color: filter === 'dinner' ? 'white' : '#292929' }}
-        >DINNER</button>
-        <button 
-          className={'tag-nav-button'} 
-          onClick={() => {filterByTag('')}}
-          style={{ color: filter === '' ? 'white' : '#292929' }}
-        >ALL</button>
-        {filter && <button className={'remove-filter'} onClick={() => {filterByTag('')}}>{renderFilters()}</button>}
-      </nav>
+      <FilterNav filterByTag={filterByTag} filter={filter}/>
       <section>
         {filter ? recipes.filter(r => r.tags.includes(filter)).map(r => <RecipeCard key={r.id} recipe={r} setFilter={setFilter}/>) : recipes.map(r => <RecipeCard key={r.id} recipe={r} setFilter={setFilter}/>)}
       </section>
