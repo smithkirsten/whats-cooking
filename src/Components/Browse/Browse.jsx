fa0d9ad98ef1c9911f9b802f3c9bf888e9c775bd
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-13 13:00:38 -0400

    Add button highlighting for active buttons on nav bar

--- src/Components/Browse/Browse.jsx
+++ src/Components/Browse/Browse.jsx
@@ -10,8 +10,6 @@ function Browse({user, setUser, recipes }) {
     setFilter(tag)
   }
   const renderFilters = () => {
-    console.log('rendering the filters!')
-    console.log(filter)
     if(filter !== 'breakfast' && filter !== 'lunch' && filter !== 'dinner' && filter !== '') {
       //once active class is applied to nav buttons, can simplify this conditional into checking whether any of the buttons are active
       return filter
@@ -22,10 +20,26 @@ function Browse({user, setUser, recipes }) {
     <main>
       <Header user={user} setUser={setUser} />
       <nav className={'tag-nav'}>
-        <button className={'tag-nav-button'} onClick={() => {filterByTag('breakfast')}}>BREAKFAST</button>
-        <button className={'tag-nav-button'} onClick={() => {filterByTag('lunch')}}>LUNCH</button>
-        <button className={'tag-nav-button'} onClick={() => {filterByTag('dinner')}}>DINNER</button>
-        <button className={'tag-nav-button'} onClick={() => {filterByTag('')}}>ALL</button>
+        <button 
+          className={'tag-nav-button'} 
+          onClick={() => {filterByTag('breakfast')}}
+          style={{ color: filter === 'breakfast' ? 'white' : '#292929' }}
+        >BREAKFAST</button>
+        <button 
+          className={'tag-nav-button'} 
+          onClick={() => {filterByTag('lunch')}}
+          style={{ color: filter === 'lunch' ? 'white' : '#292929' }}
+        >LUNCH</button>
+        <button 
+          className={'tag-nav-button'} 
+          onClick={() => {filterByTag('dinner')}}
+          style={{ color: filter === 'dinner' ? 'white' : '#292929' }}
+        >DINNER</button>
+        <button 
+          className={'tag-nav-button'} 
+          onClick={() => {filterByTag('')}}
+          style={{ color: filter === '' ? 'white' : '#292929' }}
+        >ALL</button>
         {filter && <button className={'remove-filter'} onClick={() => {filterByTag('')}}>{renderFilters()}</button>}
       </nav>
       <section>
