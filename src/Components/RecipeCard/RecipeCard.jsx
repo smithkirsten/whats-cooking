2c2f621cdabd38c4351ddf6a13c174fc9f5be2a0
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-07-17 13:09:05 -0400

    Center and resize image

--- src/Components/RecipeCard/RecipeCard.jsx
+++ src/Components/RecipeCard/RecipeCard.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import Header from '../Header/Header'
+import './RecipeCard.css'
 
 
 function RecipeCard({ recipe, setFilter }) {
  
   return (
     <article className={'recipe-card'}>
-      <img src={recipe.image} alt={recipe.name} className={'recipe-image'}/>
-
+      <div className={'image-container'}>
+        <img src={recipe.image} alt={recipe.name} className={'recipe-image'}/>
+      </div>
       <section className={'recipe-deets'}>
         <h2>{recipe.name}</h2>
         {recipe.ingredients.length < 11 && <p className={'easy-meal'}>less than 10 ingredients!</p>}
         <div className={'tags-section'}>
           {recipe.tags.map(tag => <button className={'tag'} onClick={() => {setFilter(tag)}}>#{tag}</button>)}
         </div>
+        <button>Add to my Cookbook</button>
       </section>
-
     </article>
   );
 }
